62e95361eb2562994f82ecbb5a309b7a7fc452a0
Claire Scanlon <104991589+clairescanlon@users.noreply.github.com>
2024-03-25 20:25:37 +0000

    Uploading files for project and starting work on project

--- simple-aged-cache/initialcapacity-simple-aged-cache-aa4622f5b005d40e51ada16a82607cb86b3be5fd/src/main/java/io/collective/SimpleAgedCache.java
+++ simple-aged-cache/initialcapacity-simple-aged-cache-aa4622f5b005d40e51ada16a82607cb86b3be5fd/src/main/java/io/collective/SimpleAgedCache.java
@@ -3,14 +3,32 @@ package io.collective;
 import java.time.Clock;
 
 public class SimpleAgedCache {
+    private final ExpirableEntry[] entries;
+    private final Clock clock;
+    private int size;
 
     public SimpleAgedCache(Clock clock) {
+        this.clock = clock;
+        this.entries = new ExpirableEntry[16]; // initial capacity of 16
+        this.size = 0;
     }
 
     public SimpleAgedCache() {
+        this(Clock.systemDefaultZone());
     }
 
     public void put(Object key, Object value, int retentionInMillis) {
+        ExpirableEntry<Object, Object> entry = new ExpirableEntry<>(key, value, retentionInMillis, clock);
+  
+        // Check to see if array needs to be resized
+        if (size== entries.length) {
+            ExpirableEntry[] newEntries = new ExpirableEntry[entries.length * 2];
+            System.arraycopy(entries, 0, newEntries, 0, entries.length);
+            entries = newEntries;
+        }
+        
+        //add the new entry to the array
+        entries[size++] = entry;
     }
 
     public boolean isEmpty() {
