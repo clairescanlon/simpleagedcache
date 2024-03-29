541c565f50743fdb4633210e5e2d6f998bfa35a9
Claire Scanlon <104991589+clairescanlon@users.noreply.github.com>
2024-03-28 20:28:53 -0400

    revisions made

--- simple-aged-cache/initialcapacity-simple-aged-cache-aa4622f5b005d40e51ada16a82607cb86b3be5fd/src/main/java/io/collective/SimpleAgedCache.java
+++ simple-aged-cache/initialcapacity-simple-aged-cache-aa4622f5b005d40e51ada16a82607cb86b3be5fd/src/main/java/io/collective/SimpleAgedCache.java
@@ -22,4 +22,49 @@ public class SimpleAgedCache<K,V> {
         this(Clock.systemDefaultZone(), expirationDuration, expirationTimeUnit);
     }
 
-   
+   public SimpleAgedCache(long expirationDuration, TimeUnit expirationTimeUnit) {
+    this(Clock.systemDefaultZone(), expirationDuration, expirationTimeUnit);
+   }
+
+   public void put(K key, V value){
+    cache.put(key, new ExpirableEntry<>(key, value, expirationDuration, expirationTimeUnit, clock));
+   }
+
+   public boolean isEmpty() {
+    return cache.isEmpty();
+   }
+
+   public int size() {
+    return cache.size();
+   }
+
+   public V get(K key) {
+    Expirableentry<K, V> entry = cache.get(key);
+    if (entry != null && !entry.isExpired(clock.millis())) {
+        return entry.getValue();
+    }
+    return null;
+   }
+
+private static class ExpirableEntry<K, V> {
+    private final K key;
+    private final V value;
+    private final long expirationTime;
+
+    ExpirableEntry(K key, V value, long expirationDuration, TimeUnit expirationTimeUnit, Clock clock) {
+        this.key = key;
+        this.value = value;
+        this.expirationTime = clock.millis() + expirationTimeUnit.toMillis(expirationDuration); 
+    }
+    K getKey() {
+        return key;
+    }
+
+    V getValue() {
+        return value;
+    }
+
+    boolean isExpired(long currentTime) {
+        return currentTime >= expirationTime;
+    }
+}
